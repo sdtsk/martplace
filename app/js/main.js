8b28b25f40f7aa84b91eb4136177f101075a48c5
sdtsk <dotsenko_tsk@mail.ru>
2020-07-13 17:51:52 +0700

    added advantages/slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
-
-
+    
+    $('.products__slider').slick({
+        prevArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-right"></span></button>'
+    });
 
 });
