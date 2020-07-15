21d61b44fff27b3db3a871fc20b4e94032346d13
sdtsk <dotsenko_tsk@mail.ru>
2020-07-15 20:29:01 +0700

    added followers/optimized other

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,37 @@
 $(function(){
+
+    $(".products__rating").rateYo({
+        starWidth: "15px",
+        ratedFill: "#ffc000",
+        rating: 4.5,
+        readOnly: true
+    });
+
+    $(".new-products__item-bottom-rating").rateYo({
+        starWidth: "12px",
+        ratedFill: "#ffc000",
+        rating: 4.5,
+        readOnly: true
+    });
+
+    $(".followers__item-bottom-rating").rateYo({
+        starWidth: "12px",
+        ratedFill: "#ffc000",
+        rating: 4.5,
+        readOnly: true
+    });
     
     $('.products__slider').slick({
+        appendArrows: $('.products__title-btn'),
+        prevArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-right"></span></button>'
+    });
+
+    $('.followers__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        appendArrows: $('.followers__title-btn'),
         prevArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
         nextArrow: '<button class="slick-prev"><span class="lnr lnr-chevron-right"></span></button>'
     });
